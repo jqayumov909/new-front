953b55c5469752c295a4e39a162a6babb4925af3
jqayumov909 <jqayumov909@gmail.com>
2023-07-03 01:00:40 +0500

    add js

---
+++ lesson-one/js/script.js
@@ -0,0 +1,8 @@
+document.getElementById("demo").innerHTML = 5 + 6;
+document.write(5 + 6);
+window.alert(5 + 6);
+console.log(5 + 6);
+function myFunction() {
+  document.getElementById("demo1").innerHTML = "Hello Dolly!";
+  document.getElementById("demo2").innerHTML = "How are you?";
+}
