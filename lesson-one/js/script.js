3b41bb3d892fdc7766a403dd669c64e8b57f1e95
jqayumov909 <jqayumov909@gmail.com>
2023-07-04 15:02:47 +0500

    all changes saved

--- lesson-one/js/script.js
+++ lesson-one/js/script.js
@@ -6,3 +6,6 @@ function myFunction() {
   document.getElementById("demo1").innerHTML = "Hello Dolly!";
   document.getElementById("demo2").innerHTML = "How are you?";
 }
+function surNay() {
+  document.getElementById("sw").innerHTML = "";
+}
